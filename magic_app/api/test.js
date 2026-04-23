f440780b986587ddf048a465aaecd8f18a19bbb8
alexmel669-dotcom <alexmel669@gmail.com>
2026-04-24 01:58:15 +0500

    Delete magic_app/api/test.js

--- magic_app/api/test.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-    res.status(200).json({ 
-        status: 'ok', 
-        message: 'API работает!',
-        time: new Date().toISOString()
-    });
-}
