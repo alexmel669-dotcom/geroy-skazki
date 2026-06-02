a9030413cb4fa93db5e7318fe711861c4d4e50bf
alexmel669-dotcom <alexmel669@gmail.com>
2026-06-02 23:48:31 +0500

    Update cors.js

--- magic_app/api/_middleware/cors.js
+++ magic_app/api/_middleware/cors.js
@@ -1,1 +1,16 @@
-
+export function setCors(req, res) {
+  const origin = process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:3000' 
+    : 'https://geroy-skazki.vercel.app';
+  
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return true;
+  }
+  return false;
+}
